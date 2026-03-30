114053f866f3bdc9a250662eda55b5b23e6c780b
Dilan Dilaruksha <dilandilruksha0@gmail.com>
2026-03-30 15:08:19 +0530

    Remove request and volunteer services along with their configurations and database migrations

--- apps/web/lib/api/urls.ts
+++
@@ -1,8 +0,0 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3005";
-
-export const SERVICE_URLS = {
-  auth: API_BASE,
-  request: API_BASE,
-  volunteer: API_BASE,
-  notification: API_BASE
-};
